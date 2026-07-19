20d71b0c9453ea9a9aaa3a59a9381044b689f4f3
walidzaaneun <zaanoun3@gmail.com>
2026-07-19 14:38:49 +0100

    add commandLineRunner for persist sample data

--- src/main/java/com/walidzaaneun/digitalbankingbackend/DigitalBankingBackendApplication.java
+++ src/main/java/com/walidzaaneun/digitalbankingbackend/DigitalBankingBackendApplication.java
@@ -1,7 +1,22 @@
 package com.walidzaaneun.digitalbankingbackend;
 
+import com.walidzaaneun.digitalbankingbackend.entities.AccountOperation;
+import com.walidzaaneun.digitalbankingbackend.entities.CurrentAccount;
+import com.walidzaaneun.digitalbankingbackend.entities.Customer;
+import com.walidzaaneun.digitalbankingbackend.entities.SavingAccount;
+import com.walidzaaneun.digitalbankingbackend.enums.AccountStatus;
+import com.walidzaaneun.digitalbankingbackend.enums.OperationType;
+import com.walidzaaneun.digitalbankingbackend.repositories.AccountOperationRepository;
+import com.walidzaaneun.digitalbankingbackend.repositories.BankAccountRepository;
+import com.walidzaaneun.digitalbankingbackend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DigitalBankingBackendApplication {
@@ -10,4 +25,49 @@ public class DigitalBankingBackendApplication {
         SpringApplication.run(DigitalBankingBackendApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository,
+                            CustomerRepository customerRepository) {
+        return args -> {
+            Stream.of("Walid","Issa","Sanae").forEach(name->{
+                Customer customer=new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"@gmail.com");
+                customerRepository.save(customer);
+            });
+
+            customerRepository.findAll().forEach(cust->{
+                CurrentAccount currentAccount=new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(Math.random()*90000);
+                currentAccount.setCreatedAt(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(cust);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+                SavingAccount savingAccount=new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(Math.random()*90000);
+                savingAccount.setCreatedAt(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(cust);
+                savingAccount.setInterestRate(5.5);
+                bankAccountRepository.save(savingAccount);
+
+            });
+
+            bankAccountRepository.findAll().forEach(acc->{
+                for (int i = 0; i <10 ; i++) {
+                    AccountOperation accountOperation=new AccountOperation();
+                    accountOperation.setOperationDate(new Date());
+                    accountOperation.setAmount(Math.random()*12000);
+                    accountOperation.setType(Math.random()>0.5? OperationType.DEBIT: OperationType.CREDIT);
+                    accountOperation.setBankAccount(acc);
+                    accountOperationRepository.save(accountOperation);
+                }
+            });
+        };
+    }
 }
