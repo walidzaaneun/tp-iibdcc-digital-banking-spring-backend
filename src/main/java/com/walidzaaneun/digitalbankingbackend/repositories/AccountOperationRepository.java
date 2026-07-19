07ccc0ef59f04bbc5aec91327a9b5b7e935a6633
walidzaaneun <zaanoun3@gmail.com>
2026-07-19 15:44:24 +0100

    update repositories

--- src/main/java/com/walidzaaneun/digitalbankingbackend/repositories/AccountOperationRepository.java
+++ src/main/java/com/walidzaaneun/digitalbankingbackend/repositories/AccountOperationRepository.java
@@ -1,7 +1,12 @@
 package com.walidzaaneun.digitalbankingbackend.repositories;
 
 import com.walidzaaneun.digitalbankingbackend.entities.AccountOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface AccountOperationRepository extends JpaRepository<AccountOperation,Long> {
+    List<AccountOperation> findByBankAccountId(String accountId);
+    Page<AccountOperation> findByBankAccountIdOrderByOperationDateDesc(String accountId, Pageable pageable);
 }
